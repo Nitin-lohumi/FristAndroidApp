8fd3208e64ed1ec7baf3a65ecfcd3a4d4c272ef3
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-08-22 13:45:46 +0530

    ad

--- app/src/main/java/com/example/app1/MainActivity.kt
+++ app/src/main/java/com/example/app1/MainActivity.kt
@@ -1,34 +1,62 @@
 package com.example.app1
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
-    lateinit var diceImage:ImageView;
+    lateinit var diceImage:ImageView
+    lateinit var body: View
+    lateinit var changeMode:Button
+    lateinit var clickbait: Button
+    lateinit var textView:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.main_activity);
-        val clickbait: Button = findViewById(R.id.button);
-        diceImage=findViewById(R.id.imageView);
+        setContentView(R.layout.main_activity)
+        textView = findViewById(R.id.textView)
+        changeMode = findViewById(R.id.changeModeButton)
+        body =  findViewById(R.id.body)
+        changeMode.setOnClickListener {
+            modes()
+        }
+        clickbait = findViewById(R.id.button)
+        diceImage=findViewById(R.id.imageView)
         clickbait.setOnClickListener {
-            exchange();
+            exchange()
         }
     }
-    private fun exchange() {
-        val randomNum = Random.nextInt(1,7);
-        val diceNumber  = when(randomNum){
-              1->R.drawable.dice_1;
-              2->R.drawable.dice_2;
-              3->R.drawable.dice_3;
-              4->R.drawable.dice_4;
-              5->R.drawable.dice_5;
-             else -> R.drawable.dice_6;
+
+    private fun modes() {
+        if(changeMode.text.equals("dark Mode")){
+            body.setBackgroundResource(R.color.black)
+            textView.setBackgroundResource(R.color.white)
+            changeMode.text="Light mode"
+            textView.text ="this is dark mode"
+        }else{
+            changeMode.text="dark Mode"
+            textView.text ="Dice Rollar"
+            textView.setBackgroundResource(R.color.black)
+            body.setBackgroundResource(R.color.white)
+            changeMode.setBackgroundResource(R.color.black)
         }
-        diceImage.setImageResource(diceNumber);
+    }
+
+    private fun exchange() {
+        val randomNum = Random.nextInt(1,7)
+        val diceNumber  = when(randomNum){
+              1->R.drawable.dice_1
+            2->R.drawable.dice_2
+            3->R.drawable.dice_3
+            4->R.drawable.dice_4
+            5->R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(diceNumber)
     }
 }
 
